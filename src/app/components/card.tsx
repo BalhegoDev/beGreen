acd77012eec67eb071e157cfb1d0c669f9c80fff
BalhegoDev <adgamesbalhego@hotmail.com>
2024-10-19 18:48:38 -0300

    main page fineshed

---
+++ src/app/components/card.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+
+
+type props = {
+    image:string,
+    title:string
+    link:string
+}
+
+export default function Card({image,title,link}: props):ReactNode{
+    return(
+        <article className=" flex flex-col items-center m-auto gap-3">
+            <a href={link} rel="noopener noreferrer">
+                <section className=" h-20 w-20 rounded-2xl flex items-center justify-center overflow-hidden border-2 border-secondaryGreen">
+                    <Image
+                        src={image}
+                        alt="Imagem ilustrativa"    
+                        width={80}
+                        height={80}
+                    />
+                </section>
+            </a>
+            <p className="text-secondaryGreen font-bold">{title}</p>
+        </article>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,54 @@
 import Image from "next/image";
-import Form from "./components/loginForm";
+import Card from "./components/card";
 
 export default function Home() {
   return (
     <>
-
+       <header>
+          <h1 className="text-secondaryGreen font-bold mb-20 text-4xl">
+            Olá, o que você está procurando hoje?
+          </h1>
+       </header>
+       <article className="block m-auto">
+          <Image
+            src="/droneMain.svg"
+            alt="Imagem de um drone"
+            width={400}
+            height={400}
+          />
+       </article>
+       <article className="flex flex-wrap gap-4">
+          <Card
+            image="/ambiental.svg"
+            title="Ambiental"
+            link="/ambiente"
+          />
+          <Card
+            image="/geografico.svg"
+            title="Geográfico"
+            link="/"
+          />
+          <Card
+            image="/pessoas.svg"
+            title="Populacional"
+            link="/"
+          />
+          <Card
+            image="/turismo.svg"
+            title="Turismo"
+            link="/"
+          />
+          <Card
+            image="/comercio.svg"
+            title="Comércio"
+            link="/"
+          />
+          <Card
+            image="/outro.svg"
+            title="Outras opções"
+            link="/"
+          />
+       </article>
     </>
   );
 }
